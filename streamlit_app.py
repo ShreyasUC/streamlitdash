9a192713bf1d068cbfbe060273d505c56f06e18c
ShreyasUC <shreyas@acefour.in>
2024-11-05 17:58:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import pandas as pd
 import math
 from pathlib import Path
 
-@st.cache_data
-# Instead of a CSV on disk, you could read from an HTTP endpoint here too.
 DATA_FILENAME = Path(__file__).parent/'data/base.csv'
 raw_gdp_df = pd.read_csv(DATA_FILENAME)
 
