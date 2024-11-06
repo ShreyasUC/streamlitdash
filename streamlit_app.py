a3b1d86b81e886617bddcee4b63f87b261c52dc3
ShreyasUC <shreyas@acefour.in>
2024-11-06 17:27:42 +0530

    Revenue trend

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,7 +183,8 @@ with col4:
     st.markdown(f"""
         <div style="padding: 15px; background-color: #f1f1f1; border-radius: 8px; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
             <h3 style="text-align: center; font-size: 18px;">
-                DRR (GMV): ₹{drr_gmv:,.0f} Lakhs
+                DRR (GMV): ₹{drr_gmv:,.0f} 
+                <h5> Lakhs </h5>
                 <span style="font-size: 13px; color: {arrow_color};">
                     {arrow} {abs(rt):.2f}%
                 </span>
