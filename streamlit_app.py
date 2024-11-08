08a7ac5ba01e534f869d524b8c4f4d6fb1537219
ShreyasUC <shreyas@acefour.in>
2024-11-08 13:15:52 +0530

    Multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ end_date = st.sidebar.date_input("End Date", df['order-date'].max())
 
 # Add an "All" option for filters in the sidebar
 #category_filter = st.sidebar.selectbox('Select Category', ['All'] + list(df['category'].unique()))
-category_filter = st.multiselect('Select Category', ['All'] + list(df['Platform'].unique()), default=['All'])
+category_filter = st.multiselect('Select Category', ['All'] + list(df['category'].unique()), default=['All'])
 
 zone_filter = st.sidebar.selectbox('Select Customer Zone', ['All'] + list(df['cust-zone'].unique()))
 platform_filter = st.sidebar.selectbox('Select Platform', ['All'] + list(df['platform'].unique()))
