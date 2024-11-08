1bd9c6dff9e364c1994bd20606a73909f26272f2
ShreyasUC <shreyas@acefour.in>
2024-11-08 11:29:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,7 @@ last_month_df = df[(df['order-date'] >= first_day_last_month) &
                              (df['order-date'] <= last_day_last_month)]
 last_month_revenue = last_month_df['revenue'].sum()
 divisor = last_day_last_month.day
-lm_rev_drr = last_month_revenue/divisor/100000
+lm_rev_drr = last_month_revenue/divisor
 revenue_trend = drr_gmv - lm_rev_drr
 rt = revenue_trend/lm_rev_drr*100
 
